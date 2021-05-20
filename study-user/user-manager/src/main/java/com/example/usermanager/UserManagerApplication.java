4496f86d6232409d7b50eb89749248c1fcd1acae
zhongjing <zhongjing@flashhold.com>
2021-05-20 16:19:33 +0800

    add manager model

--- study-user/user-manager/src/main/java/com/example/usermanager/UserManagerApplication.java
+++ study-user/user-manager/src/main/java/com/example/usermanager/UserManagerApplication.java
@@ -3,6 +3,9 @@ package com.example.usermanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author zhongjing
+ */
 @SpringBootApplication
 public class UserManagerApplication {
 
