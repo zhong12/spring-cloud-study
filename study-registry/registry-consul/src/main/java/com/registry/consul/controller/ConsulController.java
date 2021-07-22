f430eaed3b3221a92fe9b7cf00b0f39d11886eac
zhongjing <zhongjing@flashhold.com>
2021-07-22 17:41:37 +0800

    add registry-consul

--- study-registry/registry-consul/src/main/java/com/registry/consul/controller/ConsulController.java
+++ study-registry/registry-consul/src/main/java/com/registry/consul/controller/ConsulController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class ConsulController {
 
     @RequestMapping("/hello")
     public String hello() {
